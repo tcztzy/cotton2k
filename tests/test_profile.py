124293cef073abf67325ee182a5afbc35c84e7b1
Tang Ziya <tcztzy@gmail.com>
2020-08-11 17:59:59 +0800

    rewrite test for profile

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -1,21 +1,9 @@
 import unittest
-import tempfile
 import datetime
-from unittest.case import skip
-from cotton2k.io import parse_profile, parse_profile_carbon_dioxide, parse_profile_description, parse_profile_output_flags, parse_profile_simulation_dates
+from cotton2k.io import parse_profile, parse_profile_carbon_dioxide, parse_profile_description, parse_profile_field, parse_profile_geometry, parse_profile_output_flags, parse_profile_output_options, parse_profile_parameter_files, parse_profile_simulation_dates, parse_profile_weather
 
 
 class ProfileFileTestCase(unittest.TestCase):
-    content = """\
-HAKB1.PRO           HAZOR 1984 experiment, treatment KB1               
-08-APR-1984    01-APR-1984    28-SEP-1984                                       
-REHA84.ACT                                                                      
-HAZOR.HYD           HAZOR.INT           HAKB1.AGI                               
-    32.000    35.000    50.000         1
-    96.520     0.000    10.000         4
-        10    20-APR-1984    20-SEP-1984        10    01-JUN-1984    20-SEP-1984    
-  0  0  1  1  0  1  1  1  1  1  1  1  0  0  0  0  1  0  0  0  0  0  0"""
-
     def test_description(self):
         line = 'HAKB1.PRO           HAZOR 1984 experiment, treatment KB1               '
         self.assertEqual(parse_profile_description(line)['description'], 'HAZOR 1984 experiment, treatment KB1')
@@ -41,33 +29,33 @@ HAZOR.HYD           HAZOR.INT           HAKB1.AGI
         with self.assertRaises(ValueError):
             parse_profile_carbon_dioxide('     1.000  101  100')
 
-    def test_line_3(self):
-        result = parse_profile(self.content)
+    def test_weather(self):
+        result = parse_profile_weather("REHA84.ACT                                                                      ")
         self.assertEqual(result.get('actualWeatherFileName'), 'REHA84.ACT')
 
-    def test_line_4(self):
-        result = parse_profile(self.content)
+    def test_parameter_files(self):
+        result = parse_profile_parameter_files("HAZOR.HYD           HAZOR.INT           HAKB1.AGI                               ")
         self.assertEqual(result.get('soilHydraulicFileName'), 'HAZOR.HYD')
         self.assertEqual(result.get('soilInitFileName'), 'HAZOR.INT')
         self.assertEqual(result.get('agriculturalInputFileName'), 'HAKB1.AGI')
         self.assertEqual(result.get('plantmapFileName'), '')
 
-    def test_line_5(self):
-        result = parse_profile(self.content)
+    def test_geometry(self):
+        result = parse_profile_geometry("    32.000    35.000    50.000         1")
         self.assertEqual(result.get('latitude'), 32.000)
         self.assertEqual(result.get('longitude'), 35.000)
         self.assertEqual(result.get('elevation'), 50.000)
         self.assertEqual(result.get('siteNumber'), 1)
 
-    def test_line_6(self):
-        result = parse_profile(self.content)
+    def test_field(self):
+        result = parse_profile_field("    96.520     0.000    10.000         4")
         self.assertEqual(result['rowSpace'], 96.520)
         self.assertEqual(result['skipRowWidth'], 0.000)
         self.assertEqual(result['plantsPerMeter'], 10.000)
         self.assertEqual(result['varNumber'], 4)
 
-    def test_line_7(self):
-        result = parse_profile(self.content)
+    def test_output_options(self):
+        result = parse_profile_output_options("        10    20-APR-1984    20-SEP-1984        10    01-JUN-1984    20-SEP-1984    ")
         self.assertEqual(result['soilMapFrequency'], 10)
         self.assertEqual(result['soilMapStartDate'], datetime.date(1984, 4, 20))
         self.assertEqual(result['soilMapEndDate'], datetime.date(1984, 9, 20))
