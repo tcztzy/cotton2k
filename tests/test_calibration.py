a20a321066e715c0321da6903e54f04052432011
Tang Ziya <tcztzy@gmail.com>
2020-08-14 19:20:09 +0800

    rewrite test for calibration

--- tests/test_calibration.py
+++ tests/test_calibration.py
@@ -1,12 +1,9 @@
-import unittest
-
 from cotton2k.io import parse_list_dat, parse_parameter
 
 
-class CalibrationTestCase(unittest.TestCase):
-    def test_list_dat(self):
-        result = parse_list_dat(
-            """   2 GC510                              VPARGC510.DAT
+def test_list_dat():
+    result = parse_list_dat(
+        """   2 GC510                              VPARGC510.DAT
    3 MAXXA                              VPARMAXXA.DAT
    4 ACALA SJ2                          VPARSJ2.DAT
    5 SIVON                              VPARSIVON.DAT
@@ -16,37 +13,38 @@ class CalibrationTestCase(unittest.TestCase):
   18 China Test1                        VPARTest1_xlz8.DAT
   19 China Test2                        VPARTest2_xlz8.DAT
   20 China Test3                        VPARTest3_xlz8.DAT"""
-        )
-        self.assertEqual(result[2], ("GC510", "VPARGC510.DAT"))
-        self.assertEqual(result[3], ("MAXXA", "VPARMAXXA.DAT"))
-        self.assertEqual(result[4], ("ACALA SJ2", "VPARSJ2.DAT"))
-        self.assertEqual(result[5], ("SIVON", "VPARSIVON.DAT"))
-        self.assertEqual(result[11], ("DELTAPINE 61", "VPARDP61.DAT"))
-        self.assertEqual(result[12], ("DELTAPINE 77", "VPARDP77.DAT"))
-        self.assertEqual(result[17], ("新陆早8号", "VPARxlz8.DAT"))
-        self.assertEqual(result[18], ("China Test1", "VPARTest1_xlz8.DAT"))
-        self.assertEqual(result[19], ("China Test2", "VPARTest2_xlz8.DAT"))
-        self.assertEqual(result[20], ("China Test3", "VPARTest3_xlz8.DAT"))
-        result = parse_list_dat(
-            """   0 CALIFORNIA, West SJ                USACALSJWEST.DAT   
+    )
+    assert result[2] == ("GC510", "VPARGC510.DAT")
+    assert result[3] == ("MAXXA", "VPARMAXXA.DAT")
+    assert result[4] == ("ACALA SJ2", "VPARSJ2.DAT")
+    assert result[5] == ("SIVON", "VPARSIVON.DAT")
+    assert result[11] == ("DELTAPINE 61", "VPARDP61.DAT")
+    assert result[12] == ("DELTAPINE 77", "VPARDP77.DAT")
+    assert result[17] == ("新陆早8号", "VPARxlz8.DAT")
+    assert result[18] == ("China Test1", "VPARTest1_xlz8.DAT")
+    assert result[19] == ("China Test2", "VPARTest2_xlz8.DAT")
+    assert result[20] == ("China Test3", "VPARTest3_xlz8.DAT")
+    result = parse_list_dat(
+        """   0 CALIFORNIA, West SJ                USACALSJWEST.DAT   
    1 ISRAEL, Coastal                    ILCOASTAL.DAT
    2 ARIZONA, Phoenix                   USAAZCENTRAL.DAT
    3 ISRAEL, Galil                      ILGALIL.DAT 
    4 ISRAEL, Avdat                      ILAVDAT.DAT 
 """
-        )
-        self.assertEqual(result[0], ("CALIFORNIA, West SJ", "USACALSJWEST.DAT"))
-        self.assertEqual(result[1], ("ISRAEL, Coastal", "ILCOASTAL.DAT"))
-        self.assertEqual(result[2], ("ARIZONA, Phoenix", "USAAZCENTRAL.DAT"))
-        self.assertEqual(result[3], ("ISRAEL, Galil", "ILGALIL.DAT"))
-        self.assertEqual(result[4], ("ISRAEL, Avdat", "ILAVDAT.DAT"))
+    )
+    assert result[0] == ("CALIFORNIA, West SJ", "USACALSJWEST.DAT")
+    assert result[1] == ("ISRAEL, Coastal", "ILCOASTAL.DAT")
+    assert result[2] == ("ARIZONA, Phoenix", "USAAZCENTRAL.DAT")
+    assert result[3] == ("ISRAEL, Galil", "ILGALIL.DAT")
+    assert result[4] == ("ISRAEL, Avdat", "ILAVDAT.DAT")
 
-    def test_parameter(self):
-        result = parse_parameter(
-            """Test
+
+def test_parameter():
+    result = parse_parameter(
+        """Test
    0.050       Whatever
    0.049       Yet another""",
-            2,
-        )
-        self.assertEqual(result[0], 0.050)
-        self.assertEqual(result[1], 0.049)
+        2,
+    )
+    assert result[0] == 0.050
+    assert result[1] == 0.049
