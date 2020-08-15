5eac22a47c05f17ea3d346854794fe1b05ad22e5
Tang Ziya <tcztzy@gmail.com>
2020-08-15 23:24:12 +0800

    version update

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ Programming Language :: Python :: 3.8"""
 
 setup(
     name="cotton2k",
-    version="2020.08.14",
+    version="2020.08.15",
     author="Tang Ziya",
     author_email="tcztzy@gmail.com",
     description="Reimplementation for Cotton2k simulation model",
