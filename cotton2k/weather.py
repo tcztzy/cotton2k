d085061811efbac77e7b617ae67edcb07c076ba0
Tang Ziya <tcztzy@gmail.com>
2020-08-18 23:46:16 +0800

    weather

--- cotton2k/weather.py
+++ cotton2k/weather.py
@@ -1,7 +1,20 @@
 from dataclasses import dataclass
 from typing import List
+from pathlib import Path
+from re import findall
 
+
+def parse_weather(content: str):
+    lines = content.splitlines()
+    clim = map(lambda line: findall('.{7}', line[21:]), lines[1:])
+    return dict(
+        clim=clim
+    )
 
 @dataclass
 class Weather:
     clim: List
+
+    @classmethod
+    def from_file(cls, path: Path):
+        return cls(**parse_weather(path.read_text()))
