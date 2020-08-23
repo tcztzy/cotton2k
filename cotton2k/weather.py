57eb350b7af1524fc098167d55a414608d6cfb93
Tang Ziya <tcztzy@gmail.com>
2020-08-23 11:48:21 +0800

    update

--- cotton2k/weather.py
+++ cotton2k/weather.py
@@ -2,14 +2,28 @@ from dataclasses import dataclass
 from typing import List
 from pathlib import Path
 from re import findall
+from locale import atof
+
+
+def read_climate_data():
+    pass
 
 
 def parse_weather(content: str):
     lines = content.splitlines()
-    clim = map(lambda line: findall('.{7}', line[21:]), lines[1:])
-    return dict(
-        clim=clim
-    )
+    clim = map(lambda line: Climate(*map(atof, findall(".{7}", line[21:]))), lines[1:])
+    return dict(clim=clim)
+
+
+@dataclass
+class Climate:
+    radiation: float
+    max_temperature: float
+    min_temperature: float
+    rain: float
+    wind: float
+    dew_temperature: float
+
 
 @dataclass
 class Weather:
