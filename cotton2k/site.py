e72c02ff45c35bb2ee9548e1c5a13a7e1424cc16
Tang Ziya <tcztzy@hotmail.com>
2020-09-30 13:43:31 +0800

    site parameters range validation

--- cotton2k/site.py
+++ cotton2k/site.py
@@ -1,28 +1,82 @@
+"""Site-specific data processing"""
 from __future__ import annotations
 
-from dataclasses import dataclass
+from functools import wraps
+from operator import itemgetter
 from pathlib import Path
-from typing import List
+from typing import List, Optional, Tuple
 
 from cotton2k.io import parse_parameter
 
 
-@dataclass
+def parameter(item):
+    return property(itemgetter(item))
+
+
+PARAMETER_RANGES = (
+    (0, 2),
+    (2, 4),
+    (0, 2.5),
+    (0.0025, 0.0080),
+    (6.264, 20),
+    (14.554, 24.302),
+    (40, 120),
+    (1, 3.5),
+    (20, 24),
+    (3, 6.5),
+    (180, 180),
+)
+
+
 class Site:
     _site_par: List[float]
-    wind_start_hours_after_sunraise: float
-    wind_max_hours_after_noon: float
-    wind_stop_hours_after_sunset: float
+    wind_start_hours_after_sunrise: float = parameter(1)
+    wind_max_hours_after_noon: float = parameter(2)
+    wind_stop_hours_after_sunset: float = parameter(3)
+    night_wind_factor: float = parameter(4)
+    dew_temperature_when_max_temperture_over_40: float = parameter(5)
+    dew_temperature_when_max_temperture_below_20: float = parameter(6)
+    cloud_correction: float = parameter(7)
+    hours_after_noon_reatch_max_temperature: float = parameter(8)
+    deep_soil_temperature_offset: float = parameter(9)
+    deep_soil_temperature_gain: float = parameter(10)
+    deep_soil_temperature_shift: float = parameter(11)
+
+    @property
+    def parameters(self):
+        class _:
+            def __getitem__(myself, item):
+                if item <= 0:
+                    raise KeyError
+                return self._site_par[item - 1]
+
+            def __setitem__(myself, key, value):
+                i = key - 1
+                lower, upper = PARAMETER_RANGES[i]
+                if value > upper or value < lower:
+                    raise ValueError
+                self._site_par[i] = value
+
+        return _()
+
+    @parameters.setter
+    def parameters(self, value):
+        for i, v in enumerate(value):
+            lower, upper = PARAMETER_RANGES[i]
+            if v > upper or v < lower:
+                raise ValueError(f"Site[{i+1}] is {v}, not in range ({lower}, {upper})")
+        self._site_par = value
 
     @classmethod
     def from_dat(cls, site_dat_path: Path) -> Site:
         site_par = parse_parameter(site_dat_path.read_text(), 16)
-        return cls(
-            _site_par=site_par,
-            wind_start_hours_after_sunraise=site_par[0],
-            wind_max_hours_after_noon=site_par[1],
-            wind_stop_hours_after_sunset=site_par[2],
-        )
+        return cls(site_par)
+
+    def __init__(self, _site_par):
+        self.parameters = _site_par
 
     def __getitem__(self, item):
-        return self._site_par[item - 1]
+        return self.parameters[item]
+
+    def __setitem__(self, key, value):
+        self.parameters[key] = value
--- tests/test_site.py
+++ tests/test_site.py
@@ -11,8 +11,9 @@ def site_dat(tmp_path: Path):
     lines = [
         "Test site",
         "{:>8.1f}".format(1),
-        "{:>8.1f}".format(1),
+        "{:>8.1f}".format(2),
         "{:>8.1f}".format(2.5),
+        "{:>8.4f}".format(0.0060),
     ]
     dat.write_text("\n".join(lines))
     return dat
@@ -20,7 +21,30 @@ def site_dat(tmp_path: Path):
 
 def test_site(site_dat):
     site = Site.from_dat(site_dat)
-    assert site[1] == site.wind_start_hours_after_sunraise
+    assert site[1] == site.wind_start_hours_after_sunrise
     assert site[1] == 1.00
-    assert site[2] == 1.00
+    assert site[2] == 2.00
     assert site[3] == 2.5
+    assert site[4] == 0.0060
+    with pytest.raises(KeyError):
+        site[0]
+    site[1] = 2.00
+    assert site._site_par[0] == 2.00 and site[1] == 2.00
+    with pytest.raises(ValueError):
+        site[1] = 3.00
+
+
+@pytest.fixture
+def invalid_site_dat(tmp_path: Path):
+    dat = tmp_path / "test_site.dat"
+    lines = [
+        "Invalid site",
+        "{:>8.1f}".format(3),
+    ]
+    dat.write_text("\n".join(lines))
+    return dat
+
+
+def test_invalid_site(invalid_site_dat):
+    with pytest.raises(ValueError):
+        site = Site.from_dat(invalid_site_dat)
