e8a289ca54b6273eed0f3d52b1c71dfd3cbcf163
Tang Ziya <tcztzy@hotmail.com>
2020-10-11 18:13:09 +0800

    test for date_to_day_of_year

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,10 +2,15 @@ from datetime import date
 
 import pytest
 
-from cotton2k.utils import strptime
+from cotton2k.utils import date_to_day_of_year, strptime
 
 
 def test_strptime():
     assert strptime("01-OCT-2020") == date(2020, 10, 1)
     with pytest.raises(ValueError):
         strptime("Whatever")
+
+
+def test_date_to_day_of_year():
+    assert date_to_day_of_year(date(2020, 10, 1)) == 275
+    assert date_to_day_of_year(date(2020, 1, 1), 2019) == 366
