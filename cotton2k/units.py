b85afaee9e48d9195eee89c1d382de965296bbbd
Tang Ziya <tcztzy@gmail.com>
2020-10-24 12:48:02 +0800

    Introduce length unit

--- cotton2k/units.py
+++ cotton2k/units.py
@@ -2,11 +2,13 @@
 
 from __future__ import annotations
 
-from decimal import Decimal
 from dataclasses import dataclass
-from typing import Union
+from decimal import Decimal
+from fractions import Fraction
+from typing import Any, Union
 
-Number = Union[float, int, Decimal]  # pylint: disable=unsubscriptable-object
+Number = Union[int, Decimal, Fraction]  # pylint: disable=unsubscriptable-object
+AnyNumber = Union[float, Number]  # pylint: disable=unsubscriptable-object
 
 
 @dataclass
@@ -14,34 +16,60 @@ class Quantity:
     """Quantity class"""
 
     value: Number
-    unit: TemperatureUnit
+    unit: Unit
 
     def __eq__(self, o: object) -> bool:
         if not isinstance(o, Quantity):
             raise TypeError
         if self.unit == o.unit:
             return self.value == o.value
-        else:
-            self_value = (self.value - self.unit.offset) / self.unit.gain
-            o_value = (o.value - o.unit.offset) / o.unit.gain
-            return self_value == o_value
+        self_value = (self.value - self.unit.offset) / self.unit.gain
+        o_value = (o.value - o.unit.offset) / o.unit.gain
+        return self_value == o_value
 
 
 @dataclass
-class TemperatureUnit:
-    """TemperatureUnit class"""
+class Unit:
+    """Unit class"""
 
-    gain: Number
-    offset: Number
+    gain: Number = 1
+    offset: Number = 0
 
-    def __rmul__(self, other):
-        return Quantity(other, self)
-
+    def __setattr__(self, name: str, value: Any) -> None:
+        if name not in ["gain", "offset"]:
+            ...
+        elif not isinstance(value, (int, float, Decimal)):
+            raise TypeError
+        elif isinstance(value, float):
+            value = Decimal(str(value))
+        super().__setattr__(name, value)
 
     def __eq__(self, o: object) -> bool:
-        if not isinstance(o, TemperatureUnit):
+        if not isinstance(o, Unit):
             raise TypeError
+        if self.__class__ != o.__class__:
+            raise TypeError(f"{type(self)} can't compare with {type(o)}")
         return self.gain == o.gain and self.offset == o.offset
 
-degree_Celsius = TemperatureUnit(1, Decimal("-273.15"))
-degree_Fahrenheit = TemperatureUnit(Decimal("1.8"), Decimal("-459.67"))
+    def __rmul__(self, other) -> Quantity:
+        if isinstance(other, float):
+            value = Decimal(str(other))
+        else:
+            value = other
+        return Quantity(value, self)
+
+
+class TemperatureUnit(Unit):  # pylint: disable=too-few-public-methods
+    """TemperatureUnit class"""
+
+
+degree_Kelvin = K = TemperatureUnit()
+degree_Celsius = C = TemperatureUnit(1, Decimal("-273.15"))
+degree_Fahrenheit = F = TemperatureUnit(Decimal("1.8"), Decimal("-459.67"))
+
+
+class LengthUnit(Unit):  # pylint: disable=too-few-public-methods
+    """LengthUnit class"""
+
+
+meter = m = LengthUnit()
--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,6 +1,21 @@
 import pytest
 
-from cotton2k.units import degree_Celsius, degree_Fahrenheit
+from cotton2k.units import (
+    LengthUnit,
+    degree_Celsius,
+    degree_Fahrenheit,
+    degree_Kelvin,
+    meter,
+)
+
+
+def test_unit():
+    with pytest.raises(TypeError, match="can't compare"):
+        assert meter == degree_Celsius
+    唐 = LengthUnit(1.78, 0)  # author's height
+    唐.name = "唐梓涯的身高"
+    with pytest.raises(TypeError):
+        唐.gain = None
 
 
 def test_temperature_unit():
@@ -16,3 +31,4 @@ def test_temperature_unit():
     assert zero_degree_Celsius == 0 * degree_Celsius
     assert zero_degree_Celsius == 32 * degree_Fahrenheit
     assert 100 * degree_Celsius == 212 * degree_Fahrenheit
+    assert 0 * degree_Kelvin == -273.15 * degree_Celsius
