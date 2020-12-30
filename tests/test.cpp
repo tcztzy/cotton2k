ac6edf3661b7cea3c8b756cc4ad1f8018ac5dc0e
Tang Ziya <tcztzy@gmail.com>
2020-12-30 15:48:30 +0800

    TEST: SoilTemOnRootGrowth

--- tests/test.cpp
+++ tests/test.cpp
@@ -16,3 +16,16 @@ TEST(GeneralFunctions, DateToDoy) {
 TEST(GeneralFunctions, DoyToDate) {
     EXPECT_EQ(DoyToDate(275, 2020), "01-OCT-2020");
 }
+double SoilTemOnRootGrowth(double);
+TEST(RootGrowth, SoilTemOnRootGrowth) {
+    EXPECT_EQ(SoilTemOnRootGrowth(30), 1);
+    EXPECT_NEAR(SoilTemOnRootGrowth(28), 0.9712, 1e-5);
+    EXPECT_NEAR(SoilTemOnRootGrowth(26), 0.9168, 1e-5);
+    EXPECT_NEAR(SoilTemOnRootGrowth(24), 0.8368, 1e-5);
+    EXPECT_NEAR(SoilTemOnRootGrowth(22), 0.7312, 1e-5);
+    EXPECT_NEAR(SoilTemOnRootGrowth(20), 0.6000, 1e-5);
+    EXPECT_NEAR(SoilTemOnRootGrowth(18), 0.4432, 1e-5);
+    EXPECT_NEAR(SoilTemOnRootGrowth(16), 0.2608, 1e-5);
+    EXPECT_NEAR(SoilTemOnRootGrowth(14), 0.0528, 1e-5);
+    EXPECT_EQ(SoilTemOnRootGrowth(13.5), 0);
+}
