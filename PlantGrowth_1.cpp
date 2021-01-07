cc2eca2bfd3d0795542fd4d98859f613d9798bef
Tang Ziya <tcztzy@gmail.com>
2021-01-07 10:50:02 +0800

    REFACTOR RUST: LeafResistance

--- PlantGrowth_1.cpp
+++ PlantGrowth_1.cpp
@@ -14,7 +14,9 @@
 
 void LeafWaterPotential(const string &, const double &, const int &);
 
-double LeafResistance(double);
+extern "C" {
+    double LeafResistance(double);
+}
 
 // PlantGrowth_2
 void PotentialLeafGrowth(const double &);
@@ -299,31 +301,6 @@ void LeafWaterPotential(const string &ProfileName, const double &PlantHeight, co
         File44 << LwpMin;
         File44 << endl;
     }
-}
-
-//////////////////////////////////
-double LeafResistance(double agel)
-//     This function computes and returns the resistance of leaves of cotton
-// plants to transpiration. It is assumed to be a function of leaf age. 
-// It is called from LeafWaterPotential().
-//     The input argument (agel) is leaf age in physiological days.
-{
-//     The following constant parameters are used:
-    const double afac = 160;  // factor used for computing leaf resistance.
-    const double agehi = 94;  // higher limit for leaf age.
-    const double agelo = 48;  // lower limit for leaf age.
-    const double rlmin = 0.5; // minimum leaf resistance.
-//
-    double leafResistance;
-    if (agel <= agelo)
-        leafResistance = rlmin;
-    else if (agel >= agehi)
-        leafResistance = rlmin + (agehi - agelo) * (agehi - agelo) / afac;
-    else {
-        double ax = 2 * agehi - agelo; // intermediate variable
-        leafResistance = rlmin + (agel - agelo) * (ax - agel) / afac;
-    }
-    return leafResistance;
 }
 
 ////////////////////////////////
