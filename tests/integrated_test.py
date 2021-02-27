2047641526d307d80c6d10ba7b0c713c22610af7
Tang Ziya <tcztzy@gmail.com>
2021-02-27 14:07:35 +0800

    TEST: remove the outputs after test

--- tests/integrated_test.py
+++ tests/integrated_test.py
@@ -1,28 +1,54 @@
 #!/usr/bin/env python
 import os
+import re
 import sys
-
 from argparse import ArgumentParser
 from datetime import date
 from difflib import context_diff
 from itertools import product
 from pathlib import Path
-import re
 
 today = date.today().strftime("%A, %B %d, %Y").ljust(30)
-extensions = ["B01", "F01", "PLT", "S01", "SMP", "WA2", "WAT", "LWP", "CHB", "RUT", "TM1", "TM2", "TMS", "NB0", "NB1", "NB2", "NB3", "NB4"]
+extensions = [
+    "B01",
+    "F01",
+    "PLT",
+    "S01",
+    "SMP",
+    "WA2",
+    "WAT",
+    "LWP",
+    "CHB",
+    "RUT",
+    "TM1",
+    "TM2",
+    "TMS",
+    "NB0",
+    "NB1",
+    "NB2",
+    "NB3",
+    "NB4",
+]
 fixtures = Path(__file__).parent / "fixtures"
 parser = ArgumentParser()
-parser.add_argument("output_dir", type=Path, default=Path('.'))
-parser.add_argument("--profiles", metavar="P", nargs="+", default=["hakb1", "hava98"], help="Profiles to be compared")
+parser.add_argument("output_dir", type=Path, default=Path("."))
+parser.add_argument(
+    "--profiles",
+    metavar="P",
+    nargs="+",
+    default=["hakb1", "hava98"],
+    help="Profiles to be compared",
+)
+
 
 def replace_zero(lines: list[str]):
     result = []
     for line in lines:
-        if re.match('^(  [1-9]| [1-3]\d| 40)', line):
-            line = line[:3] + line[3:].replace('0', ' ', -1)
+        if re.match("^(  [1-9]| [1-3]\d| 40)", line):
+            line = line[:3] + line[3:].replace("0", " ", -1)
         result.append(line)
     return result
+
 
 def main(output_dir: Path, profiles: list[str]):
     for pro, ext in product(profiles, extensions):
@@ -40,6 +66,7 @@ def main(output_dir: Path, profiles: list[str]):
                 a = replace_zero(a)
                 b = replace_zero(b)
             sys.stdout.writelines(context_diff(a, b))
+            output.unlink()
 
 
 if __name__ == "__main__":
