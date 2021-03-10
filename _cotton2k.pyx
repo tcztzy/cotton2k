2a964a32c240dc71bc2862374aba746fbf47764a
Tang Ziya <tcztzy@gmail.com>
2021-03-10 11:35:18 +0800

    REFACTOR: seperate input output features to _io.pxd

--- _cotton2k.pyx
+++ _cotton2k.pyx
@@ -7,31 +7,7 @@ from datetime import datetime, date
 
 from _global cimport *
 from _structs cimport *
-
-cdef extern from "GettingInput_1.cpp":
-    void ReadCalibrationData()
-    void InitializeGrid(Simulation &)
-    double PlantsPerM
-    double SkipRowWidth
-    int nSiteNum
-    string SiteName
-    int nVarNum
-    string VarName
-
-cdef extern from "GettingInput_2.cpp":
-    void ReadSoilImpedance(Simulation &)
-    void InitSoil(const string &)
-    void InitializeSoilTemperature()
-    void InitializeSoilData(Simulation &, const string &)
-    void InitializeRootData(Simulation &)
-
-cdef extern from "gettingInput_3.cpp":
-    int OpenClimateFile(const string &, const string &, const int &, ClimateStruct[400])
-    void ReadAgriculturalInput(Simulation &, const string &)
-
-cdef extern from "Output.h":
-    void DataOutput(Simulation &)
-    void OpenOutputFiles(const char *, const char *, int, int);
+from _io cimport *
 
 cdef extern from "Cottonmodel.h":
 
---
+++ _io.pxd
@@ -0,0 +1,28 @@
+from libcpp.string cimport string
+
+from _structs cimport Simulation, ClimateStruct
+
+cdef extern from "GettingInput_1.cpp":
+    void ReadCalibrationData()
+    void InitializeGrid(Simulation &)
+    double PlantsPerM
+    double SkipRowWidth
+    int nSiteNum
+    string SiteName
+    int nVarNum
+    string VarName
+
+cdef extern from "GettingInput_2.cpp":
+    void ReadSoilImpedance(Simulation &)
+    void InitSoil(const string &)
+    void InitializeSoilTemperature()
+    void InitializeSoilData(Simulation &, const string &)
+    void InitializeRootData(Simulation &)
+
+cdef extern from "gettingInput_3.cpp":
+    int OpenClimateFile(const string &, const string &, const int &, ClimateStruct[400])
+    void ReadAgriculturalInput(Simulation &, const string &)
+
+cdef extern from "Output.h":
+    void DataOutput(Simulation &)
+    void OpenOutputFiles(const char *, const char *, int, int);
