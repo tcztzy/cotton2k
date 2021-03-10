9f5ad3f9d7a9a2a57bcbd3d49ae921fc765cf079
Tang Ziya <tcztzy@gmail.com>
2021-03-10 11:27:55 +0800

    REFACTOR: seperate structs to _structs.pxd

--- _cotton2k.pyx
+++ _cotton2k.pyx
@@ -6,41 +6,7 @@ from libc.stdlib cimport malloc
 from datetime import datetime, date
 
 from _global cimport *
-
-cdef extern from "State.h":
-    ctypedef struct State:
-        double plant_height
-        double plant_weight
-        double lint_yield
-
-cdef extern from "Climate.h":
-    ctypedef struct ClimateStruct:
-        pass
-
-cdef extern from "Simulation.h":
-    ctypedef struct Simulation:
-        const char *profile_name
-        int year
-        unsigned int day_start
-        unsigned int day_finish
-        unsigned int day_emerge
-        unsigned int day_plant
-        unsigned int day_start_soil_maps
-        unsigned int day_stop_soil_maps
-        unsigned int day_start_co2
-        unsigned int day_end_co2
-        double co2_enrichment_factor
-        unsigned int day_start_mulch
-        unsigned int day_end_mulch
-        unsigned int mulch_indicator
-        double mulch_transmissivity_short_wave
-        double mulch_transmissivity_long_wave
-        double latitude
-        double longitude
-        double elevation
-        double row_space
-        State *states
-        ClimateStruct climate[400]
+from _structs cimport *
 
 cdef extern from "GettingInput_1.cpp":
     void ReadCalibrationData()
