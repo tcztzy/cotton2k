3010ea4b76c7a91306f9008603c35c52f2a66731
Tang Ziya <tcztzy@gmail.com>
2021-03-24 11:50:45 +0800

    DROP: unused exceptions

--- exceptions.h
+++ exceptions.h
@@ -1,35 +1,13 @@
 #ifndef EXCEPTIONS_H
 #define EXCEPTIONS_H
 #include <stdexcept>
-#include <filesystem>
-namespace fs = std::filesystem;
 
 class Cotton2KException
     : public std::runtime_error
 {
 public:
     Cotton2KException(const std::string &_Message)
-        : std::runtime_error(_Message.c_str())
-    {
-    }
-};
-
-class FileNotExists
-    : public Cotton2KException
-{
-public:
-    FileNotExists(const fs::path filePath) noexcept
-        : Cotton2KException("The file  " + filePath.string() + "  does not exist!")
-    {
-    }
-};
-
-class FileNotOpened
-    : public Cotton2KException
-{
-public:
-    FileNotOpened(const fs::path filePath) noexcept
-        : Cotton2KException("Can't open " + filePath.string() + ".")
+        : std::runtime_error(_Message)
     {
     }
 };
@@ -44,7 +22,7 @@ public:
     }
 
     SimulationEnd(const std::string &_Message) noexcept
-        : std::runtime_error(_Message.c_str())
+        : std::runtime_error(_Message)
     {
     }
 };
