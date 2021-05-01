0e5a0d683deff328ac0000212da3368019ec3cfc
Tang Ziya <tcztzy@gmail.com>
2021-05-01 18:23:32 +0800

    TEST: test coverage up

--- tests/test_io.py
+++ tests/test_io.py
@@ -6,3 +6,5 @@ from cotton2k.io import read_input
 def test_read_input(empty_json):
     with pytest.raises((KeyError, TypeError)):
         read_input(empty_json)
+    with pytest.raises((KeyError, TypeError)):
+        read_input({})
