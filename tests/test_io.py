6f3324382661fb2e78943b990bf857b0d6b59092
Tang Ziya <tcztzy@gmail.com>
2021-05-01 18:47:33 +0800

    TEST: raise test coverage to 90%

--- tests/test_io.py
+++ tests/test_io.py
@@ -3,8 +3,11 @@ import pytest
 from cotton2k.io import read_input
 
 
-def test_read_input(empty_json):
+def test_read_input(empty_json, test_json):
     with pytest.raises((KeyError, TypeError)):
         read_input(empty_json)
     with pytest.raises((KeyError, TypeError)):
         read_input({})
+    with pytest.raises((KeyError, TypeError)):
+        read_input(str(empty_json))
+    read_input(test_json)
