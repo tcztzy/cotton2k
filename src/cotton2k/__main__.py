2644807314bd14cab34e32400635e48e70774cc4
Tang Ziya <tcztzy@hotmail.com>
2021-06-21 20:52:14 +0800

    REFACTOR: prog entry point

--- src/cotton2k/__main__.py
+++ src/cotton2k/__main__.py
@@ -1,8 +1,21 @@
+import argparse
 import pathlib
-import sys
 
 from cotton2k import run
 
-if len(sys.argv) != 2:
-    sys.exit("Usage:\ncotton2k <profile name>\n")
-run(pathlib.Path(sys.argv[1]))
+parser = argparse.ArgumentParser()
+parser.add_argument("profile", help="Profile file path")
+parser.add_argument(
+    "--output",
+    action="store_true",
+    help="Dump simulation result to user data directory.",
+)
+args = parser.parse_args()
+profile = args.profile
+if (path := pathlib.Path(profile)).exists():
+    run(path, output=args.output)
+else:
+    raise NotImplementedError(
+        f"File {path} cannot be found.\n"
+        "Currently, cotton2k only support run simulation from profile file."
+    )
