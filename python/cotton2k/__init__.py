c5c67d0da7996936c38a67ab8c7fd649392c617e
Tang Ziya <tcztzy@gmail.Com>
2022-03-04 16:55:59 +0800

    PYTHON: run now take PathLike

---
+++ python/cotton2k/__init__.py
@@ -0,0 +1,17 @@
+"""\
+Cotton2K simulation model
+=========================
+
+```python
+import cotton2k as c2k
+
+c2k.run("/path/to/your/profile.toml")
+```
+"""
+from os import PathLike
+from .cotton2k import run as _run
+
+__all__ = ("run",)
+
+def run(profile: PathLike):
+    _run(str(profile))
